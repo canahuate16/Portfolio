c203c10c4380b36cb0daa612e85e4b163cc6e868
Luis Canahuate <luis.canahuate16@gmail.com>
2020-12-06 20:44:55 -0500

    updated service section

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Gamer", "Developer", "Thinker", "Designer", "Freelancer"],
+    strings: ["Developer", "Gamer", "Thinker", "Designer", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
